ebd40e49fa2c377d80e92524fa1bb9145cb49789
José <kratos1054@gmail.com>
2023-11-26 18:54:08 +0100

    Finalizado ejercicio 5, siguiente

--- Ejercicios/Ejercicio5Unidad2/Ejercicio5Unidad2Recepcion.php
+++ Ejercicios/Ejercicio5Unidad2/Ejercicio5Unidad2Recepcion.php
@@ -10,6 +10,72 @@
  Deberá mostrarlos en una tabla, con labels que pongan las claves y sus respectivos valores.
  Realizar 2 versiones de la función, en uno utilizando funciones para realizar el cálculo (explode, max, min)
   y otro sin utilizar ninguna función. */
+
+function tablaNumeros($numeros, $orden = true)
+{
+    $nprimos = calculoPrimos($numeros);
+    $media = calculoMedia($numeros);
+    $minimo = calculoMinimo($numeros);
+
+    $resultado = [
+        "nprimos" => $nprimos,
+        "media" => $media,
+        "minimo" => $minimo
+    ];
+    //Si $orden pasado como parámetro es false, se invierte el orden del array asociativo:
+    if (!$orden) {
+        $resultado = array_reverse($resultado);
+    }
+
+    return $resultado;
+}
+function calculoPrimos($array)
+{
+    //Le pasamos como Callback la función que verifica si el elemento del array pasado como parámetro es primo.
+    return count(array_filter($array, "esPrimo"));
+}
+function esPrimo($num)
+{
+    if ($num > 1) {
+        // Verificar si el número es divisible por algún número menor que él
+        for ($i = 2; $i < $num; $i++) {
+            if ($num % $i == 0) {
+                return false; // No es primo
+            }
+        }
+        return true; // Es primo
+    } else {
+        return false; // No es primo, porque $num es 1 o menor.
+    }
+}
+
+function calculoMedia($array)
+{
+    return count($array) > 0 ? array_sum($array) / count($array) : 0;
+}
+function calculoMinimo($array)
+{
+    return count($array) > 0 ? min($array) : 0;
+}
 if (isset($_POST["numeros"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
-    $numeros = $_POST["numeros"];
+    $numerosString = $_POST["numeros"];
+
+    // Eliminar espacios al principio y al final de la cadena
+    $numerosString = trim($numerosString);
+
+    // Dividir la cadena en un array usando espacios
+    $numerosArray = preg_split('/\s+/', $numerosString);
+
+    // Filtrar elementos vacíos del array
+    $numerosArray = array_filter($numerosArray);
+
+    print_r($numerosArray);
+    $resultado = tablaNumeros($numerosArray);
+    // Mostrar los resultados en una tabla
+    echo '<table border="1">';
+    echo '<tr><th>Clave</th><th>Valor</th></tr>';
+    foreach ($resultado as $clave => $valor) {
+        echo "<tr><td>{$clave}</td><td>{$valor}</td></tr>";
+    }
+    echo '</table>';
 }
